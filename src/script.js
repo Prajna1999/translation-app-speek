05d3f8bc7bed87f31d4eabae7479eea9cd78285b
prajna prayas <32980302+Prajna1999@users.noreply.github.com>
2022-04-26 11:01:36 +0530

    feat: english and hindi as default languages

--- src/script.js
+++ src/script.js
@@ -7,7 +7,15 @@ selectTag.forEach((tag,id)=>{
   
     for(let country_code in countries){
         
-        let option=`<option value="${country_code}">${countries[country_code]}</option>`
+        let selected;
+        if(id==0){
+            selected=country_code=="en-GB"?"selected":"";
+        }else if(id==1){
+            selected=country_code=="hi-IN"?"selected":""
+        }
+
+
+        let option=`<option value="${country_code}" ${selected}>${countries[country_code]}</option>`
         tag.insertAdjacentHTML("beforeend", option);
     }
     
